66d483cb712cb6e7db3dd721693e7ff51b417c6a
muto ryoko <obng3prn3@gmail.com>
2025-06-27 00:45:27 +0900

    fixd route, solved 404

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/products', [ProductController::class, 'index'])->name('products');
-Route::get('/products/{productId}', [ProductController::class, 'show'])->name('detail');
-Route::patch('/products/{productId}/update', [ProductController::class, 'update'])->name('update');
+Route::get('/products/search', [ProductSearchController::class, 'search'])->name('search');
 Route::get('/products/register', [ProductController::class, 'register'])->name('register');
 Route::post('/products', [ProductController::class, 'store'])->name('store');
-Route::get('/products/search', [ProductSearchController::class, 'search'])->name('search');
+Route::get('/products/{productId}', [ProductController::class, 'show'])->name('detail');
+Route::patch('/products/{productId}/update', [ProductController::class, 'update'])->name('update');
 Route::delete('/products/{productId}', [ProductController::class, 'destroy'])->name('delete');
