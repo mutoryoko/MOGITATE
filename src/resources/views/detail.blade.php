e1317b26b6e81271e44b85d62fb907112f6f226f
muto ryoko <obng3prn3@gmail.com>
2025-06-26 16:14:24 +0900

    edit detail.css, blade

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -7,57 +7,69 @@
 @section('title', '商品詳細')
 
 @section('content')
-<p>商品一覧 > 商品名</p>
 <div class="detail__content">
+    <p>商品一覧 > 商品名</p>
     <form action="{{ route('update', ['productId' => $product->id])}}" method="post">
         @csrf
         @method('PATCH')
-        <div class="detail__flex">
-            <div class="detail__img-wrapper"
-                <img class="detail__img" src="{{ asset($product->image) }}" alt="{{ $product->name }}">
-                <input type="file" name="image" accept=".png, .jpeg, .jpg, image/png, image/jpg">
+        <div class="detail__content--upper">
+            <div class="detail__img-wrapper">
+                <div class="detail__img">
+                    <img src="{{ asset($product->image) }}" alt="{{ $product->name }}">
+                </div>
+                <div class="detail__img--upload">
+                    <input type="file" name="image" accept=".png, .jpeg, .jpg, image/png, image/jpg">
+                </div>
                 @error('image')
                     <p class="error">{{ $message }}</p>
                 @enderror
             </div>
             <div class="detail__name-price-season">
-                <label>商品名
-                    <input type="hidden" name="id" value="{{ $product->id }}">
-                    <input type="text" name="name" value="{{ old('name', $product->name) }}" >
-                </label>
-                @error('name')
-                    <p class="error">{{ $message }}</p>
-                @enderror
-                <label>値段
-                    <input type="text" name="price" value="{{ old('price', $product->price) }}">
-                </label>
-                @error('price')
-                    <p class="error">{{ $message }}</p>
-                @enderror
-                <label>季節
-                    @foreach ($seasons as $season)
+                <div class="detail__name">
+                    <label><div>商品名</div>
+                        <input type="hidden" name="id" value="{{ $product->id }}">
+                        <input type="text" name="name" value="{{ old('name', $product->name) }}" >
+                    </label>
+                    @error('name')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="detail__price">
+                    <label><div>値段</div>
+                        <input type="text" name="price" value="{{ old('price', $product->price) }}">
+                    </label>
+                    @error('price')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="detail___seasons">
+                    <label><div>季節</div>
+                        @foreach ($seasons as $season)
                         <input type="checkbox" id="{{ $season->id }}" name="seasons[]" value="{{ $season->id }}"
                         @if (in_array( $season->id, old('seasons', $product->seasons->pluck('id')->all())))
                         checked @endif>
                         <label class="season__label" for="{{ $season->id }}">{{ $season->name }}</label>
-                    @endforeach
+                        @endforeach
+                    </label>
+                    @error('seasons')
+                    <p class="error">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="detail__content--lower">
+            <div class="detail__description">
+                <label><div>商品詳細</div>
+                    <textarea class="detail__description--text" name="description">{{ $product->description }}</textarea>
                 </label>
-                @error('seasons')
+                @error('description')
                     <p class="error">{{ $message }}</p>
                 @enderror
             </div>
-        </div>
-        <div class="detail__description">
-            <label>商品詳細
-                <textarea name="description" cols="30" rows="10">{{ $product->description }}</textarea>
-            </label>
-            @error('description')
-                <p class="error">{{ $message }}</p>
-            @enderror
-        </div>
-        <div class="back-update__buttons">
-            <a class="back__btn" href="{{ route('products') }}">戻る</a>
-            <button class="update__btn" type="submit">変更を保存</button>
+            <div class="back-update__buttons">
+                <a class="back__btn" href="{{ route('products') }}">戻る</a>
+                <button class="update__btn" type="submit">変更を保存</button>
+            </div>
         </div>
     </form>
     <div class="delete__btn">
